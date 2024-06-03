a09a5d1ab6c2cf0e0411dc944bc43ffa9f61814b
Arya Dimas <sariroti22990@gmail.com>
2024-06-03 16:28:58 +0700

    penyesuaian tampilan halaman registrasi

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -5,9 +5,17 @@ class Auth extends CI_Controller
 {
     public function index()
     {
-        $data['title'] = 'Halaman Login';
+        $data['title'] = 'Login Page';
         $this->load->view('templates/auth_header', $data);
         $this->load->view('auth/login');
+        $this->load->view('templates/auth_footer');
+    }
+
+    public function registration()
+    {
+        $data['title'] = 'Registration Page';
+        $this->load->view('templates/auth_header', $data);
+        $this->load->view('auth/registration');
         $this->load->view('templates/auth_footer');
     }
 }
