df8b3cca80dfe4e51b93144a21746e9555e40f97
Raihan Yuanda <demeehh3@gmail.com>
2024-06-03 16:38:13 +0700

    backend fitur login

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -5,10 +5,45 @@ class Auth extends CI_Controller
 {
     public function index()
     {
-        $data['title'] = 'Login Page';
-        $this->load->view('templates/auth_header', $data);
-        $this->load->view('auth/login');
-        $this->load->view('templates/auth_footer');
+        $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email');
+        $this->form_validation->set_rules('password', 'Password', 'trim|required');
+
+        if ($this->form_validation->run() == false) {
+            $data['title'] = 'Login Page';
+            $this->load->view('templates/auth_header', $data);
+            $this->load->view('auth/login');
+            $this->load->view('templates/auth_footer');
+        } else {
+            // validasinya success
+            $this->_login();
+        }
+    }
+
+
+    private function _login()
+    {
+        $email = $this->input->post('email');
+        $password = $this->input->post('password');
+
+        $user = $this->db->get_where('users', ['email' => $email])->row_array();
+        // usernya ada
+        if ($user) {
+            // cek password
+            if (password_verify($password, $user['password'])) {
+                $data = [
+                    'email' => $user['email'],
+                ];
+
+                $this->session->set_userdata($data);
+                redirect('admin');
+            } else {
+                $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Wrong password!</div>');
+                redirect('auth');
+            }
+        } else {
+            $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Email is not registered!</div>');
+            redirect('auth');
+        }
     }
 
     public function registration()
