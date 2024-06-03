2be574a9e0b871d3a58588bd36007b327bb38637
Arya Dimas <sariroti22990@gmail.com>
2024-06-03 16:33:34 +0700

    backend fitur registrasi

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -13,9 +13,34 @@ class Auth extends CI_Controller
 
     public function registration()
     {
-        $data['title'] = 'Registration Page';
-        $this->load->view('templates/auth_header', $data);
-        $this->load->view('auth/registration');
-        $this->load->view('templates/auth_footer');
+        $this->form_validation->set_rules('name', 'Name', 'required|trim');
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[users.email]', [
+            'is_unique' => 'This email has already registred!'
+        ]);
+        $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[3]|matches[password2]', [
+            'matches' => 'Password dont match!',
+            'min_length' => 'Password too short!'
+        ]);
+        $this->form_validation->set_rules('password2', 'Password', 'required|trim|matches[password1]');
+
+        if ($this->form_validation->run() == false) {
+            $data['title'] = 'Registration Page';
+            $this->load->view('templates/auth_header', $data);
+            $this->load->view('auth/registration');
+            $this->load->view('templates/auth_footer');
+        } else {
+            $email = $this->input->post('email', true);
+            $data = [
+                'name' => htmlspecialchars($this->input->post('name', true)),
+                'email' => htmlspecialchars($email),
+                'password' => password_hash($this->input->post('password1'), PASSWORD_DEFAULT),
+                'date_created' => time()
+            ];
+
+            $this->db->insert('users', $data);
+
+            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Congratulation! your account has been created. Please activate your account</div>');
+            redirect('auth');
+        }
     }
 }
